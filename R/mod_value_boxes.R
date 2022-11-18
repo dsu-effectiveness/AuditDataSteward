3cee7062ef969b9f14971a6870041ac8d1a88612
jazzyjpfeifer <Justin.Pfeifer@utahtech.edu>
2022-11-18 15:18:17 -0700

    updated cosmetics

--- R/mod_value_boxes.R
+++ R/mod_value_boxes.R
@@ -29,28 +29,28 @@ mod_value_boxes_server <- function(id, five_stats){
 
   # Colors for boxes
   n_errors_color <- "red"
-  avg_age_color <- "purple"
+  avg_age_color <- "yellow"
   n_total_color <- "blue"
-  pct_errors_color <- "olive"
-  n_tables_color <- "fuchsia"
+  pct_errors_color <- "green"
+  n_tables_color <- "orange"
 
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
     output$n_errors_valbox <- renderValueBox(
-      valueBox(value = five_stats$n_errors,
+      valueBox(value = prettyNum(five_stats$n_errors, ","),
                subtitle = "N. Errors",
                icon = icon("triangle-exclamation"),
                color = n_errors_color,
                width = 2))
     output$avg_age_valbox <- renderValueBox(
-      valueBox(value = paste(round(five_stats$avg_age), "Days"),
+      valueBox(value = prettyNum(paste(round(five_stats$avg_age), "Days"), ","),
                subtitle = "Age",
                icon = icon("calendar-day"),
                color = avg_age_color,
                width = 2))
     output$n_total_valbox <- renderValueBox(
-      valueBox(value = five_stats$n_data,
+      valueBox(value = prettyNum(five_stats$n_data, ","),
                subtitle = "Total Items",
                icon = icon("hashtag"),
                color = n_total_color,
@@ -62,7 +62,7 @@ mod_value_boxes_server <- function(id, five_stats){
                color = pct_errors_color,
                width = 2))
     output$n_tables_valbox <- renderValueBox(
-      valueBox(value = five_stats$n_tables,
+      valueBox(value = prettyNum(five_stats$n_tables, ","),
                subtitle = "N. Tables",
                icon = icon("table-list"),
                color = n_tables_color,
